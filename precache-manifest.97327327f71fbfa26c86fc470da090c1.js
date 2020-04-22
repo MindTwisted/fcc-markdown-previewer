11e21d93a01520c193d3cf5dec00fe951133eed0
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2020-04-22 17:31:32 +0300

    Updates

---
+++ precache-manifest.97327327f71fbfa26c86fc470da090c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37c1c06a23ec57d2cc0eca31d783f1e",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/index.html"
+  },
+  {
+    "revision": "4c74e53f68367095c85b",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "8a1773603e95aa55df6e",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4c74e53f68367095c85b",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/static/js/2.925eac45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/static/js/2.925eac45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1773603e95aa55df6e",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/static/js/main.0023e51c.chunk.js"
+  },
+  {
+    "revision": "023b1e53560bec5569b8",
+    "url": "https://mindtwisted.github.io/fcc-markdown-previewer/static/js/runtime-main.4b2dcafc.js"
+  }
+]);
